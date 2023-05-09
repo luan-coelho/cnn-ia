1ae356eb585f11112a541310193dda12144d8875
Luan Coêlho de Souza <luancoelho.dev@gmail.com>
2023-05-09 11:21:03 -0300

    feat: Criação do projeto.

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+from cnn import is_suspicious_request
+
+app = Flask(__name__)
+
+
+@app.before_request
+def check_request():
+    if is_suspicious_request(request):
+        return jsonify({"message": "Acesso negado. Requisição suspeita."}), 403
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
